9c7c39ac4296d8ed27655405cbbddcac9c19ce97
Akshat Patel <akshat@Akshats-MacBook-Air.local>
2025-04-29 23:46:40 +0530

    multi-page-setup

---
+++ src/CompImage.js
@@ -0,0 +1,9 @@
+import Heading from "./components/Heading/heading";
+import Image from "./components/Image/Image";
+
+const heading = new Heading();
+heading.render();
+
+const image = new Image();
+image.render();
+
---
+++ src/components/Image/Image.js
@@ -0,0 +1,14 @@
+import imag from '../../image.jpg';
+import './Image.scss';
+
+class Image{
+    render(){
+        const img = document.createElement('img')
+        img.src = imag
+        img.alt = 'Image'
+        img.classList.add('comp-image')
+        const body = document.querySelector('body')
+        body.appendChild(img)
+    }
+}
+export default Image;
